d21ba441b66aff28928cd0eb5ee75917a5332037
Matt Parkins <matt@mooflower.com>
2022-05-24 16:31:29 +0100

    config definition

--- Config.cs
+++ Config.cs
@@ -5,25 +5,30 @@ namespace moofetch {
     public enum FetchType {
         Regular,
         ExtractAndLoop,
+        PagedExtractAndLoop,
     }
 
     public class FetchItem {
         public string uri                   { get; set; } = "";
-        public FetchType fetchType          { get; set; }
-        public string extractRegex          { get; set; }
+        public FetchType fetchType          { get; set; } = FetchType.Regular;
+        public int pageStart                { get; set; } = 0;
+        public int pageIncrement            { get; set; } = 0;
+        public int pageCount                { get; set; } = 1;
+        public string extractPath           { get; set; } = "";
         public List<FetchItem> loopedItems  { get; set; }
     }
 
     public class Config {
 
-        public string dataPath          { get; set; }   // relative location for data cache
-        public bool skipFetch           { get; set; }   // force the skipping of data fetching - may crash if data isn't in the cache
-        public List<FetchItem> items    { get; set; }
+        public string dataPath              { get; set; }   // relative location for data cache
+        public bool skipFetch               { get; set; }   // force the skipping of data fetching - may crash if data isn't in the cache
+        public List<FetchItem> items        { get; set; }
         
         public static Config CreateDefault() {
             Config config = new Config();
             config.skipFetch = false;
             config.dataPath = "/data";
+
             config.items = new List<FetchItem>();
             config.items.Add(new FetchItem { uri = "http://bbc.co.uk" });
 
